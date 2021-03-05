6e4fad588f4e2679bc5dcce534b51026e3c225b7
G_EEE3 <c1as2icblu3@gmail.com>
2021-03-05 23:12:06 +0900

    ADD Chapter 2

---
+++ ch2_hw1_20171602.py
@@ -0,0 +1,16 @@
+# by GEEE3, March 5, 2021
+# 3.14를 변수에 할당한 후, 다음 순서대로 실행하는 script를 작성할 것
+
+# 1. 문자열로 변환 후, 값과 데이터 형을 출력
+a = 3.14
+a = str(a)
+print(a, type(a))
+
+# 2. 변환된 문자열을 정수로 변환 후, 값과 데이터 형을 출력
+a = float(a)
+a = int(a)
+print(a, type(a))
+
+# 3. 변환된 정수를 실수로 변환 후, 값과 데이터 형을 출력
+a = float(a)
+print(a, type(a))
---
+++ ch2_hw2_20171602.py
@@ -0,0 +1,10 @@
+# by GEEE3, March 5, 2021
+# 두개의 변수를 사용하여 변수의 값을 교환하는 script를 작성하라.
+a = 10
+b = 20
+print(a, b)
+
+a = a + b
+b = a - b
+a = a - b
+print(a, b)
